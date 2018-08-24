73171a1deafb610f1cacc2d990f168bfecc8c754
Tyler <tylerthomasallen@gmail.com>
2018-08-23 19:38:50 -0700

    Complete #set method in the Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -4,14 +4,23 @@ class Map
   end
 
   def set(key, value)
+    idx = @array.each_index { |ele| ele[0] == key }
+    if idx
+      @array[idx][1] = value
+    else
+      @array.push([key, value])
+    end
   end
 
   def get(key)
-    @array.each { |pair| return pair[1] if pair[0] == key }
+    @array.each { |ele| return ele[1] if ele[0] == key }
     nil
   end
 
   def delete(key)
+    val = get(key)
+    @array.reject! { |ele| ele[0] == key }
+    val
   end
 
   def show
