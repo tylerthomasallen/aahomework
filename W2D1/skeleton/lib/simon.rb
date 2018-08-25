c32018bd32dce23ac67d072815b666717d9e0753
Tyler <tylerthomasallen@gmail.com>
2018-08-25 11:26:57 -0700

    Complete specs for Simon skeleton

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,19 +4,29 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @game_over = false
+    @sequence_length = 1
+    @seq = []
   end
 
   def play
-
+    take_turn until game_over
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    unless game_over
+      show_sequence
+      require_sequence
+      round_success_message
+      add_random_color
+    end
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +34,8 @@ class Simon
   end
 
   def add_random_color
-
+    ran_color = COLORS.sample
+    seq << ran_color
   end
 
   def round_success_message
@@ -36,6 +47,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
