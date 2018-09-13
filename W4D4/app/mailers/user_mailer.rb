56b55b211b4a5a43afc37ee61db20f524eebc475
Tyler <tylerthomasallen@gmail.com>
2018-09-12 19:17:42 -0700

    Make view helpers and mail method for 99Cats

---
+++ W4D4/app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: user.username, subject: 'Welcome to My Awesome Site')
+  end
+end
