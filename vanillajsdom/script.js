61a95384a3e412caad751214c4be658e3972e069
Tyler <tylerthomasallen@gmail.com>
2018-09-30 12:54:44 -0700

    Start W7D1 hw

---
+++ vanillajsdom/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+
+});
