aa88df0c2661b9d67bdca4cc020f847791e8bbd4
Tyler <tylerthomasallen@gmail.com>
2018-10-02 08:38:45 -0700

    Add middleware in store/store

--- W7D2/frontend/store/store.js
+++ W7D2/frontend/store/store.js
@@ -1,12 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState,
+  applyMiddleware(addLoggingToDispatch, sillyMiddleware));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
 
 export default configureStore;
