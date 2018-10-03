7a185e2c55cab587663d3cfae398b5dbc7353a85
Tyler <tylerthomasallen@gmail.com>
2018-10-03 08:38:26 -0700

    Connect to giphy api

---
+++ W7D3/giphy/giphy_search.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import {fetchSearchGiphys} from './util/api_util';
+
+window.fetchSearchGiphys = fetchSearchGiphys;
